c158a425c3c3dda5a8b80c754a2429e293887cd1
ErickRamirezO <epramirez2@espe.edu.ec>
2024-06-29 19:50:48 -0500

    Implementación de Staggered Grid View para imágenes

--- mascotas/lib/presentation/screens/home_screen.dart
+++ mascotas/lib/presentation/screens/home_screen.dart
@@ -1,16 +1,41 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:mascotas/widgets/Background.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late List<StaggeredGridTile> items;
+
+  @override
+  void initState() {
+    super.initState();
+    generateGridItems();
+  }
+
+  void generateGridItems() {
+    items = List.generate(5, (index) {
+      int crossAxisCellCount = 2; // Valores entre 1 y 3
+      int mainAxisCellCount = Random().nextInt(3) + 1; // Valores entre 1 y 3
+      return StaggeredGridTile.count(
+        crossAxisCellCount: crossAxisCellCount,
+        mainAxisCellCount: mainAxisCellCount,
+        child: ImageWidget(index: index),
+      );
+    });
+  }
+
   // Método para actualizar las imágenes
-  void actualizarHome() {}
+  void actualizarHome() {
+    setState(() {
+      generateGridItems();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,25 +43,50 @@ class _HomeScreenState extends State<HomeScreen> {
       child: Scaffold(
         body: Stack(
           children: [
-            Background(),
+            // Background(), // No tengo acceso a tu implementación de Background, así que la he comentado temporalmente
             Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
+              padding: const EdgeInsets.all(15),
               child: ListView(
                 scrollDirection: Axis.vertical,
                 children: [
-                  const Row(
-                    children: [
-                      Text(
-                        "Home Screen",
-                        style: TextStyle(color: Colors.white, fontSize: 24),
-                      )
-                    ],
+                  SingleChildScrollView(
+                    child: StaggeredGrid.count(
+                      crossAxisCount: 4,
+                      mainAxisSpacing: 12,
+                      crossAxisSpacing: 12,
+                      children: items,
+                    ),
                   ),
                 ],
               ),
             ),
           ],
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: actualizarHome,
+          child: Icon(Icons.refresh),
+        ),
+      ),
+    );
+  }
+}
+
+class ImageWidget extends StatelessWidget {
+  final int index;
+
+  const ImageWidget({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    int photoId = Random().nextInt(30) + 55;
+    int sizeMultiplier = photoId % 7;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Image.network(
+        "https://picsum.photos/id/$photoId/300/${200 * sizeMultiplier}",
+        scale: 1,
+        fit: BoxFit.cover,
       ),
     );
   }
