ff1ce3812311680a3650bd6e76cc3d2f662e704a
Jeimy Marley Morales Sosa <99307548+jmmorales11@users.noreply.github.com>
2024-07-01 07:47:23 -0500

    Fondo

--- mascotas/lib/presentation/screens/ejemplo_screen.dart
+++ mascotas/lib/presentation/screens/ejemplo_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mascotas/widgets/Background.dart';
 
 class EjemploScreen extends StatefulWidget {
   const EjemploScreen({super.key});
@@ -12,32 +13,22 @@ class _EjemploScreenState extends State<EjemploScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          body: Container(
-        decoration: BoxDecoration(
-          gradient: RadialGradient(
-            center: Alignment.bottomRight,
-            radius: 3.5,
-            colors: [
-              const Color.fromARGB(240, 22, 61, 96),
-              const Color.fromARGB(255, 25, 23, 61),
-              const Color.fromARGB(255, 25, 23, 61),
-              const Color.fromARGB(255, 25, 23, 61),
-              const Color.fromARGB(240, 25, 23, 61),
-            ],
-          ),
-        ),
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 10),
-          child: ListView(
-            scrollDirection: Axis.vertical,
-            children: [
-              Row(
-                children: [Text("Ejmplo Screen")],
-              )
-            ],
-          ),
-        ),
-      )),
+          body: Stack(
+              //  llamar la clase Background
+              children: [
+            Background(),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: ListView(
+                scrollDirection: Axis.vertical,
+                children: [
+                  Row(
+                    children: [Text("Ejmplo Screen")],
+                  )
+                ],
+              ),
+            )
+          ])),
     );
   }
 }
--- mascotas/lib/widgets/Background.dart
+++ mascotas/lib/widgets/Background.dart
@@ -1,27 +1,21 @@
 import 'package:flutter/material.dart';
+
 class Background extends StatelessWidget {
-
-  final List<Color> colores;
-  const Background({
-    super.key,
-    this.colores = const[
-      Colors.transparent,
-      Color.fromARGB(255, 57, 156, 254),
-    ],
-  });
-
   @override
   Widget build(BuildContext context) {
     return Positioned.fill(
       child: DecoratedBox(
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: colores,
-            //Posición
-            stops: [0.6,1.0],
-            //Se pone donde se inicia y donde termina el gradiente
-            begin: Alignment.topCenter,
-            end: Alignment.bottomRight,
+          gradient: RadialGradient(
+            center: Alignment.bottomRight,
+            radius: 3.5,
+            colors: [
+              const Color.fromARGB(240, 22, 61, 96),
+              const Color.fromARGB(255, 25, 23, 61),
+              const Color.fromARGB(255, 25, 23, 61),
+              const Color.fromARGB(255, 25, 23, 61),
+              const Color.fromARGB(240, 25, 23, 61),
+            ],
           ),
         ),
       ),
