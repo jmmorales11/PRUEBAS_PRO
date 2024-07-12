931df9876df4158a022671e636cc4c96c5e99328
ErickRamirezO <epramirez2@espe.edu.ec>
2024-07-12 11:33:21 -0500

    Consumir API para obtener las imagenes de perros y mostrar en el staggered grid

--- mascotas/lib/presentation/screens/home_screen.dart
+++ mascotas/lib/presentation/screens/home_screen.dart
@@ -1,7 +1,8 @@
+import 'dart:convert';
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:mascotas/widgets/Background.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,31 +12,38 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late List<StaggeredGridTile> items;
+  late List<StaggeredGridTile> items = [];
 
   @override
   void initState() {
     super.initState();
-    generateGridItems();
+    fetchImageUrls(); // Llama al método para obtener las URLs al iniciar la pantalla
   }
 
-  void generateGridItems() {
-    items = List.generate(5, (index) {
-      int crossAxisCellCount = 2; // Valores entre 1 y 3
-      int mainAxisCellCount = Random().nextInt(3) + 1; // Valores entre 1 y 3
-      return StaggeredGridTile.count(
-        crossAxisCellCount: crossAxisCellCount,
-        mainAxisCellCount: mainAxisCellCount,
-        child: ImageWidget(index: index),
-      );
-    });
+  Future<void> fetchImageUrls() async {
+    final response = await http.get(Uri.parse('http://localhost:4000/bmpr/img/')); // Reemplaza con la URL de tu API
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
+      setState(() {
+        items = List.generate(data.length, (index) {
+          // Genera StaggeredGridTiles basados en los datos recibidos
+          int crossAxisCellCount = 2; // Valores entre 1 y 3
+          int mainAxisCellCount = Random().nextInt(3) + 1; // Valores entre 1 y 3
+          return StaggeredGridTile.count(
+            crossAxisCellCount: crossAxisCellCount,
+            mainAxisCellCount: mainAxisCellCount,
+            child: ImageWidget(index: index, imageUrl: data[index]['imagen']),
+          );
+        });
+      });
+    } else {
+      throw Exception('Failed to load image urls');
+    }
   }
 
   // Método para actualizar las imágenes
   void actualizarHome() {
-    setState(() {
-      generateGridItems();
-    });
+    fetchImageUrls(); // Llama al método para actualizar las URLs de las imágenes
   }
 
   @override
@@ -70,7 +78,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         crossAxisCount: 4,
                         mainAxisSpacing: 12,
                         crossAxisSpacing: 12,
-                        children: items,
+                        children: items != null ? items : [], // Muestra los items solo si están cargados
                       ),
                     ),
                   ],
@@ -90,18 +98,16 @@ class _HomeScreenState extends State<HomeScreen> {
 
 class ImageWidget extends StatelessWidget {
   final int index;
+  final String imageUrl;
 
-  const ImageWidget({super.key, required this.index});
+  const ImageWidget({super.key, required this.index, required this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
-    int photoId = Random().nextInt(30) + 55;
-    int sizeMultiplier = photoId % 7;
-
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: Image.network(
-        "https://picsum.photos/id/$photoId/300/${200 * sizeMultiplier}",
+        imageUrl,
         scale: 1,
         fit: BoxFit.cover,
       ),
